f0fce7a759e05b8e3ef12fa087460ea142780f7f
Nino Marrazzo <ni.marrazzo@gmail.com>
2016-07-01 09:52:47 +0200

    Minor fix and implementation

--- stepperjs-v0.1.js
+++ stepperjs-v0.1.js
@@ -2,28 +2,10 @@ function capitalise(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function handlerForType(obj, e, type) {
-  type    = capitalise(type);
-
-  switch (type) {
-    case 'Next':
-    return obj.next;
-    break;
-    case 'Prev':
-    return obj.prev;
-    break;
-    case 'Goto':
-    return obj.goto;
-    break;
-    default:
-    return 'all';
-  }
-}
-
-
-function Step(name, func) {
+function Step(name, func, number) {
   this.name = name;
   this.func = func;
+  this.previous = number;
 
   return this;
 }
@@ -43,6 +25,31 @@ function Stepper(name) {
   this.lastDirection = null;
   // Track the current direction
   this.currentDirection = null;
+  // Array containing original elements without events binded
+  this.originalNodes = [];
+  // Track the binding state
+  this.binded = false;
+
+  /**
+   * Handler to bind events
+   */
+   this.handlerForAll = function(obj, e, type) {
+     switch (type) {
+       case 'next':
+         return obj.next;
+         break;
+       case 'prev':
+         return obj.prev;
+         break;
+       case 'goto':
+         return obj.goto;
+         break;
+       default:
+        return 'all';
+     }
+   }
+
+
   /**
    * @rewindable(bool)
    * @bool: set rewind flag to true if true, false if bool is false
@@ -57,7 +64,7 @@ function Stepper(name) {
    * @func: step's func to execute
    **/
   this.step = function(name, func) {
-    this.steps.push(new Step(name, func));
+    this.steps.push(new Step(name, func, this.stepsNumber));
     this.stepsNumber++;
 
     return this;
@@ -67,17 +74,18 @@ function Stepper(name) {
    * @next()
    * Execute next step but first check if it's alredy been executed the first()
    **/
-  this.next = function() {
+  this.next = function(target) {
     if (this.currStep >= this.stepsNumber) {
       if (this.rewind)
-        this.currStep = 1;
-      this.steps[this.currStep].func();
+      this.currStep = 1;
+      this.steps[this.currStep].func(target);
     } else {
       this.currStep++;
-      this.steps[this.currStep].func();
+      this.steps[this.currStep].func(target);
     }
 
     this.lastDirection = 'next';
+
     return this;
   }
 
@@ -85,14 +93,14 @@ function Stepper(name) {
    * @prev()
    * Execute prev step
    **/
-  this.prev = function() {
-    if (this.currStep == 1) {
+  this.prev = function(target) {
+    if (this.currStep <= 1) {
       if (this.rewind)
-        this.currStep = this.stepsNumber;
+      this.currStep = this.stepsNumber;
     } else {
       this.currStep--;
     }
-    this.steps[this.currStep].func();
+    this.steps[this.currStep].func(target);
     this.lastDirection = 'prev';
 
     return this;
@@ -103,43 +111,56 @@ function Stepper(name) {
     * <sN>: step number which have to executes
     * Execute prev step
     **/
-  this.goto = function(sN) {
+  this.goto = function(target, sN) {
     this.currStep = sN;
-    this.steps[this.currStep].func();
+    this.steps[this.currStep].func(target);
+  }
+
+  this.handler = function(type, e) {
+    switch (type) {
+      case 'next':
+        return oldThis.next(e);
+        break;
+      case 'prev':
+        return oldThis.prev(e);
+        break;
+      case 'goto':
+        var gotoN = e.dataset['stexGoto'];
+        return oldThis.goto(e, gotoN);
+        break;
+    }
   }
 
   this.bindEvents = function(elements) {
     oldThis = this;
 
-    this.handlerForAll = function(obj, e, type)  {
-      switch (type) {
-        case 'next':
-          return obj.next;
-          break;
-        case 'prev':
-          return obj.prev;
-          break;
-        case 'goto':
-          var gotoN = e.dataset['stexGoto'];
-          return obj.goto;
-          break;
-      }
-    }
-
     Array.prototype.forEach.call(elements, function(e, i) {
       var type    = e.dataset['stexNext'] == undefined ? (e.dataset['stexPrev'] != undefined ? 'prev' : (e.dataset['stexGoto'] == undefined ? false : 'goto') ) : 'next';
       var trigger = e.dataset['stexTrigger'] != undefined ? e.dataset['stexTrigger'] : 'click' ;
-      e.addEventListener(trigger, oldThis.handlerForAll(oldThis, e, type));
+
+      if (!e.dataset.binded) {
+        oldThis.originalNodes[i] = e.cloneNode(true);
+
+        e.dataset.binded = true;
+
+        e.addEventListener(trigger, function (ev) {
+          oldThis.handler(type, e);
+        });
+
+      }
     });
 
     return this;
   }
 
   this.unbindEvents = function(elements) {
-    // if (funcToCall != 'all') {
     Array.prototype.forEach.call(elements, function(e, i) {
+      var type    = e.dataset['stexNext'] == undefined ? (e.dataset['stexPrev'] != undefined ? 'prev' : (e.dataset['stexGoto'] == undefined ? false : 'goto') ) : 'next';
       var trigger = e.dataset['stexTrigger'] != undefined ? e.dataset['stexTrigger'] : 'click' ;
-      e.removeEventListener(trigger, this.handlerForAll);
+      if (oldThis.originalNodes[i] == undefined) {
+        oldThis.originalNodes[i] = e;
+      }
+      e.parentNode.replaceChild(oldThis.originalNodes[i], e);
     });
 
     return this;
@@ -148,17 +169,54 @@ function Stepper(name) {
   /**
    * Rebinding function
    */
-  this.rebindEvents = function (type, elements) {
-    this.unbindEvents(elements)
-      .bindEvents(elements);
+  this.rebindEvents = function (elements) {
+    this.bindEvents(elements);
+
+    return this;
+  }
+  /**
+   * Rebinding new step's triggers
+   */
+  this.bindNew = function (elements) {
+    oldThis = this;
+
+    if (Array.isArray(elements)) {
+      Array.prototype.forEach.call(elements, function(e, i) {
+        var type    = e.dataset['stexNext'] == undefined ? (e.dataset['stexPrev'] != undefined ? 'prev' : (e.dataset['stexGoto'] == undefined ? false : 'goto') ) : 'next';
+        var trigger = e.dataset['stexTrigger'] != undefined ? e.dataset['stexTrigger'] : 'click' ;
+
+        if (!e.dataset.binded) {
+          oldThis.originalNodes.push(e.cloneNode(true));
+
+          e.dataset.binded = true;
+
+          e.addEventListener(trigger, function (ev) {
+            oldThis.handler(type, e);
+          });
+        }
+        return e;
+      });
+    } else {
+      console.log(elements);
+      var type    = elements.dataset['stexNext'] == undefined ? (elements.dataset['stexPrev'] != undefined ? 'prev' : (elements.dataset['stexGoto'] == undefined ? false : 'goto') ) : 'next';
+      var trigger = elements.dataset['stexTrigger'] != undefined ? elements.dataset['stexTrigger'] : 'click' ;
+
+      if (!elements.dataset.binded) {
+        oldThis.originalNodes.push(elements.cloneNode(true));
+
+        elements.dataset.binded = true;
+
+        elements.addEventListener(trigger, function (ev) {
+          oldThis.handler(type, elements);
+        });
+      }
+      return elements;
+    }
   }
 
   /**
    * Bind events to the in DOM triggerers
    */
-  // var tN = document.querySelectorAll('[data-stex-next]');
-  // var tP = document.querySelectorAll('[data-stex-prev]');
-  // var tG = document.querySelectorAll('[data-stex-goto]');
   this.tA = document.querySelectorAll('[data-stex-next], [data-stex-prev], [data-stex-goto]');
   this.bindEvents(this.tA);
 
